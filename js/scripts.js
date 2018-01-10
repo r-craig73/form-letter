ca8e6bcaf1e3127f5f7b8357bdf4071203b5e21a
Alex Ficklin and Ron Craig <ron@>
2018-01-10 10:17:33 -0800

    final version, with CSS, JS, HTML and bootstrap form.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#letterForm").submit(function(event) {
+    var dateInput = $("input#date").val();
+    var senderInput = $("input#sender").val();
+    var letterBodyInput= $("input#letterBody").val();
+    var yourNameInput = $("input#yourName").val();
+
+    $(".date").text(dateInput);
+    $(".sender").text(senderInput);
+    $(".letterBody").text(letterBodyInput);
+    $(".yourName").text(yourNameInput);
+
+
+    $("#returnLetter").show();
+
+    event.preventDefault();
+  });
+});
