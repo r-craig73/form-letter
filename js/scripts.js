05abfa281cbcd858bc3267a143a30dd49d056f4d
Ron Craig <ron.craig@comcast.net>
2018-09-11 18:32:43 -0700

    remove whitespace and add textbox in JS to view the letter's body

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,13 @@ $(document).ready(function() {
   $("#letterForm").submit(function(event) {
     var dateInput = $("input#date").val();
     var senderInput = $("input#sender").val();
-    var letterBodyInput= $("input#letterBody").val();
+    var letterBodyInput= $("textarea#letterBody").val();
     var yourNameInput = $("input#yourName").val();
 
     $(".date").text(dateInput);
     $(".sender").text(senderInput);
     $(".letterBody").text(letterBodyInput);
     $(".yourName").text(yourNameInput);
-
 
     $("#returnLetter").show();
 
